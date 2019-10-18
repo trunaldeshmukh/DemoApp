963b3501efe74a6e163579afa73a84e15969395a
codeSonic <mjoshi@tavisca.com>
2019-10-18 11:47:56 +0530

    added demo application with hotel listing endpoint

---
+++ DemoApp/Controllers/HotelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Hotel>> Get()
+        {
+            return new Hotel[]
+            {
+                new Hotel{Name = "MGM", CityName = "Las Vegas", CountryCode = "US", StarRating = 5},
+                new Hotel{ Name = "Venetian", CityName = "Las Vegas", CountryCode = "US", StarRating = 4.5f },
+                new Hotel{ Name = "Bellagio", CityName = "Las Vegas", CountryCode = "US", StarRating = 5 },
+                new Hotel{ Name = "Mirage", CityName = "Las Vegas", CountryCode = "US", StarRating = 4 }
+            };
+        }
+    }
+}
---
+++ DemoApp/Hotel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    public class Hotel
+    {
+        public string Name { get; set; }
+        public string CityName { get; set; }
+        public string CountryCode { get; set; }
+        public float StarRating { get; set; }
+    }
+}
